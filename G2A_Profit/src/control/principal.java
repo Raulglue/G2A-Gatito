09e8d71f1569436090e463813e8a14bf6751d338
MrHugs <hsanchezs01ex@gmail.com>
2017-04-21 12:34:23 +0100

    para el gatito* HistorialUI+botonHistorial

--- G2A_Profit/src/control/principal.java
+++ G2A_Profit/src/control/principal.java
@@ -7,6 +7,7 @@ import vista.HistorialUI;
 
 public class principal {
 	public static void main(String[] args) {
+		System.out.println("soy feo");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
